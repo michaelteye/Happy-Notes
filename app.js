8e6c769ab2526f5baef1dbb48afea1aa26bcff31
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-19 14:51:33 +0000

    adding deleting and updating to our note

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv')
 const passport = require('passport')
 const session = require('express-session')
 const morgan = require('morgan')
+const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 const MongoStore = require("connect-mongo")
@@ -17,13 +18,36 @@ require('./config/passport')(passport)
 
 connectDB()
 
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
 // Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+// Handlebars   Helpers
+const { formatDate,stripTags,truncate,editIcon,select} = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs.engine({defaultLayout:'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs.engine({
+    helpers:{formatDate, stripTags,truncate,editIcon,select},
+    defaultLayout:'main', 
+    extname: '.hbs'}))
 app.set('view engine', '.hbs')
 // seting up session
 
@@ -39,6 +63,12 @@ app.use(
 // Passport middlewares
 app.use(passport.initialize())
 app.use(passport.session()) 
+
+// Set global var
+app.use(function(req, res, next){
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
